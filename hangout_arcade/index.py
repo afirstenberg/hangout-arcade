93ada105771275cde9b85ec799cfb75da2436a7d
eslam <eslam.diaa@espace.com.eg>
2015-11-08 23:37:29 +0200

    Fixs issues, updates the README file with the steps to install firebase

--- hangout_arcade/index.py
+++ hangout_arcade/index.py
@@ -28,12 +28,55 @@ while True:
 		else:
 			get[player]['xpos'] += get[player]['xspeed']
 			get[player]['ypos'] += get[player]['yspeed']
-			firebase.patch('/spacegame/hangout2', get)
+			# print "player ....... " , player
+			# print player
+			# print get[player]['xpos']
+			# print "player : ", get[player]
+			# print '/spacegame/hangout2/'+player+'/xpos'
+			firebase.patch('/spacegame/hangout2/'+player, get[player])
+			firebase.patch('/spacegame/hangout2/'+player, get[player])
+
+			if get[player]['thrust'] == True:
+				if get[player]['rotate'] == 0 :
+					get[player]['xspeed'] += -5
+
+				elif get[player]['rotate'] == 45 :
+					get[player]['xspeed'] += -5
+					get[player]['yspeed'] += -5
+
+				elif get[player]['rotate'] == 90 :
+					get[player]['yspeed'] += -5
+
+				elif get[player]['rotate'] == 135 :
+					get[player]['xspeed'] += +5
+					get[player]['yspeed'] += -5
+
+				elif get[player]['rotate'] == 180 :
+					get[player]['xspeed'] += +5
+
+				elif get[player]['rotate'] == 225 :
+					get[player]['xspeed'] += +5
+					get[player]['yspeed'] += +5
+
+				elif get[player]['rotate'] == 270 :
+					get[player]['yspeed'] += +5
+
+				elif get[player]['rotate'] == 315 :
+					get[player]['xspeed'] += -5
+					get[player]['yspeed'] += +5
+
+				elif get[player]['rotate'] == 360 :
+					get[player]['xspeed'] += -5
+				get[player]['thrust'] = False
+				firebase.patch('/spacegame/hangout2/'+player, get[player])
+				print "........... Finished ........"
 
 		if get[player]['xpos'] > 400 or  get[player]['xpos'] < 0 or get[player]['ypos'] > 400 or get[player]['ypos'] < 0 :
+			# if it hits the boundries .. 
 			get[player]['xspeed'] *= -1
 			get[player]['yspeed'] *= -1
-			firebase.patch('/spacegame/hangout2', get)
+			firebase.patch('/spacegame/hangout2/'+player, get[player])
+
 		else:
 			# it's still inside the box ..
 			pass
