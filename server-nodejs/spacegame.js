41b9f01fb49808d3e6cbf1c87fe63e5a7b17e4a9
afirstenberg <github@addventure.com>
2015-11-21 21:47:11 -0500

    better wall rebound.

--- server-nodejs/spacegame.js
+++ server-nodejs/spacegame.js
@@ -4,28 +4,29 @@ var Firebase = require('firebase');
 var dbUrl = "https://boiling-inferno-5911.firebaseio.com/spacegame";
 var db = new Firebase( dbUrl );
 
-var processUser = function( obj ){
+var processUser = function( game, obj ){
   console.log( 'Processing User', obj.key() );
   var val = obj.val();
 
   val.xpos += val.xspeed;
   val.ypos += val.yspeed;
 
-  var bounce = false;
+  var xbounce = false;
+  var ybounce = false;
   if( val.xpos < 0 ){
-    bounce = true;
+    xbounce = true;
     val.xpos = 0;
   }
   if( val.xpos > 1000 ){
-    bounce = true;
+    xbounce = true;
     val.xpos = 1000;
   }
   if( val.ypos < 0 ){
-    bounce = true;
+    ybounce = true;
     val.ypos = 0;
   }
   if( val.ypos > 1000 ){
-    bounce = true;
+    ybounce = true;
     val.ypos = 1000;
   }
 
@@ -33,8 +34,10 @@ var processUser = function( obj ){
     xpos: val.xpos,
     ypos: val.ypos
   };
-  if( bounce ){
+  if( xbounce ){
     objChanges.xspeed = -val.xspeed;
+  }
+  if( ybounce ){
     objChanges.yspeed = -val.yspeed;
   }
 
@@ -42,18 +45,20 @@ var processUser = function( obj ){
   objDb.update( objChanges );
 };
 
-var processObject = function( obj ){
+var processObject = function( game, obj ){
   var key = obj.key();
   if( key === 'target' ){
     // TODO - handle
   } else {
-    processUser( obj );
+    processUser( game, obj );
   }
 };
 
 var processGame = function( game ){
   console.log( 'Processing Game', game.key() );
-  game.forEach( processObject );
+  game.forEach( function(obj){
+    processObject( game, obj );
+  });
 };
 
 var processAll = function( data ){
